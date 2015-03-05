1214a8f28ab98eefd16c42e49b147d7d880fcd13
Christopher Lindor <chrislindor@gmail.com>
2015-03-05 16:02:16 -0500

    Commented
    Added comments and variable to adjust number of blocks printed

--- colorBlock.js
+++ colorBlock.js
@@ -1,8 +1,16 @@
+/*
+A program that generates a random color and 
+prints it to the page the number of times specified
+*/
+
+//Set ininital variables;
 var red;
 var green;
 var blue;
 var rgbColor;
+var numberToPrint = 10;
 
+//Function to generate random colors and return rgb value;
 function colorGenerator() {
     red = Math.floor(Math.random() * 256);
     green = Math.floor(Math.random() * 256);
@@ -10,6 +18,10 @@ function colorGenerator() {
     rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
     return rgbColor;
 }
-for (var x = 1; x <= 10; x += 1) {
+
+/* for while loop to print out number 
+of blocks speciefied in numberToPrint variable
+*/
+for (var x = 1; x <= numberToPrint; x += 1) {
     document.write('<p style="background-color:' + colorGenerator() + '">' + colorGenerator() + '</p>')
 }
